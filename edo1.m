e5e01d66f4070ce4e6a613adad6fd06260a26785
teremoraiz <moraiztere@gmail.com>
2026-06-04 20:13:31 -0300

    agregue el escalon en edo1

--- edo1.m
+++ edo1.m
@@ -1,7 +1,8 @@
 %% --declaro la edo de orden 2 del ejercicio 1-- %%
 function dydt = edo1 (t,Xt,A,mp,b,a,rh,p,g,h0,k)
         
-        c1=p*g*a*h0/mp;
+        u = double(t >= 0);   % escalón unitario: 0 para t<0, 1 para t>=0
+        c1 = p*g*a*h0/mp * u;
         c2=-(b+(a^2*rh))/mp;
         c3=- ((A*k)+(p*g*a^2))/(A*mp);
         dydt=zeros(2,1);
